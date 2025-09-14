37c926455e7f197be71ac551b399ea48e0a51379
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2025-09-14 03:20:56 +0300

    Add all basic mathematic operations to be used

---
+++ script.js
@@ -0,0 +1,27 @@
+const add = function (a, b) {
+    return a + b;
+}
+
+const subtract = function (a, b) {
+    return a - b;
+}
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    if (b === 0) {
+        console.log("naughty...."); //change to an actual display output later
+    } else {
+        return a / b;
+    }
+}
+
+const power = function (a, b) {
+    return a ** b;
+}
+
+const squareRoot = function (a) {
+    return a ** (1/2);
+}
